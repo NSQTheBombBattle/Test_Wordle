6e8f8b2a679246ac4f940b1b48612ebba80689dc
NSQTheBombBattle <elina@thebombbattle.com>
2025-03-20 15:55:15 +0800

    feat: element setup

---
+++ Assets/WordGenerator.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordGenerator : MonoBehaviour
+{
+    [SerializeField] private List<string> wordSamples;
+    //[SerializeField] private List<>
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
