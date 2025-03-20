96a3ced7b4f32c2ece3981c9668e7b08b9e750f8
NSQTheBombBattle <elina@thebombbattle.com>
2025-03-20 16:20:00 +0800

    chore: pick random word

--- Assets/WordGenerator.cs
+++ Assets/WordGenerator.cs
@@ -5,16 +5,27 @@ using UnityEngine;
 public class WordGenerator : MonoBehaviour
 {
     [SerializeField] private List<string> wordSamples;
-    //[SerializeField] private List<>
+    [SerializeField] private List<Slot> slotList;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        PickRandomWord();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void PickRandomWord()
+    {
+        string targetString = wordSamples[Random.Range(0, wordSamples.Count)];
+        for (int i = 0; i < targetString.Length; i++)
+        {
+            char character = targetString[i];
+            slotList[i].textObject.text = character.ToString();
+        }
     }
 }
